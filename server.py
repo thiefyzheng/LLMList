0025405e43fe2d9ec14c5a0901d9b6db4adc1353
thiefyzheng <65657326+thiefyzheng@users.noreply.github.com>
2023-11-13 08:43:56 +0800

    I seperated html  a

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, request
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -34,47 +34,7 @@ def home():
     conn.close()
 
     # Render the ids and the ten newest models in the browser as links to their Hugging Face pages
-    return render_template_string("""
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-            }
-            .model-id {
-                display: inline-block;
-                margin: 10px;
-                padding: 20px;
-                border: 1px solid #ddd;
-                border-radius: 10px;
-                text-align: center;
-                background-color: #f9f9f9;
-            }
-            .model-id a {
-                text-decoration: none;
-                color: black;
-            }
-        </style>
-        <a href="/">Home</a>
-        <h1>Search IDs:</h1>
-        <form method="POST">
-            <input type="text" name="search" placeholder="Search..." value="{{ search_text }}">
-            <input type="submit" value="Search">
-        </form>
-        {% for id in ids %}
-            <div class="model-id">
-                <a href="https://huggingface.co/{{ id[0] }}">{{ id[0] }}</a>
-            </div>
-        {% endfor %}
-        <h1>Ten Newest Models:</h1>
-        {% for model in new_models %}
-            <div class="model-id">
-                <a href="https://huggingface.co/{{ model[0] }}">{{ model[0] }}</a>
-            </div>
-        {% endfor %}
-        <div>
-            <a href="?page={{ page - 1 }}">Previous</a>
-            <a href="?page={{ page + 1 }}">Next</a>
-        </div>
-    """, ids=ids, new_models=new_models, search_text=search_text, page=page)
+    return render_template("home.html", ids=ids, new_models=new_models, search_text=search_text, page=page)
 
 if __name__ == '__main__':
     app.run(debug=True)
